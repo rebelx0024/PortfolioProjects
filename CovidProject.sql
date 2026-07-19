f4db9e7af910754f3b8426f2280141af1e0c922e
rebelx0024 <83410025+rebelx0024@users.noreply.github.com>
2026-07-18 22:07:47 -0700

    Add files via upload

---
+++ CovidProject.sql
@@ -0,0 +1,10 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[rolling_vaccination_total]
+  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
+
+  Select *
+  From PercentPopulationVaccinated;
